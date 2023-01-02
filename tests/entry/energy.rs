fca71791fc5f6fd9f31517bd991f4f6ec29b2df4
John-John Tedro <udoprog@tedro.se>
2023-01-02 10:42:29 +0100

    Update badge, release 0.1.4

--- tests/entry/energy.rs
+++ tests/entry/energy.rs
@@ -10,7 +10,11 @@ fn test_btu_conversions() {
     assert_query!("1Gbtu^2 to J^2", 1113025000000000000000000, J ^ 2);
     assert_query!("1Gbtu^2 / 1113025kJ^2", 1000000000000);
     assert_query!("1btu^2 * 1113025J^2 to J^4", 1238824650625, J ^ 4);
-    assert_query!("1Gbtu^2 * 1113025kJ^2", 1113025000000000000000000000000, btu^2J^2);
+    assert_query!(
+        "1Gbtu^2 * 1113025kJ^2",
+        1113025000000000000000000000000,
+        btu ^ 2J ^ 2
+    );
     assert_query!(
         "1Gbtu^2 * 1113025kJ^2 to J^4",
         1238824650625000000000000000000000000,

6b7c2c9e2e02508e9c2cb594955eb9f85b91acac
John-John Tedro <udoprog@tedro.se>
2026-05-02 15:45:55 +0200

    Fix population assets

--- tools/assets/src/populations.rs
+++ tools/assets/src/populations.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::io::Cursor;
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, ensure, Context, Result};
 use anything::{Constant, Rational, Source};
 use calamine::{Data, DataType, Reader, Xlsx};
 
@@ -25,54 +26,98 @@ pub async fn download(analyzer: &Analyzer, db: &mut Db, sources: &mut Sources) -
 
     let mut doc = Xlsx::new(bytes)?;
 
-    let sheet = doc.worksheet_range("ESTIMATES")?;
+    let sheet = doc.worksheet_range("Estimates")?;
 
-    let mut it = sheet.rows().skip(12);
-    let first = it.next().context("couldn't find first row")?;
+    let mut it = sheet.rows();
 
-    let mut years = Vec::new();
-    let mut last_year = None;
-
-    for (n, col) in first.iter().enumerate().skip(7) {
-        if let Some(year) = col.as_string() {
-            let year = year.parse::<u32>()?;
-            years.push((year, n));
-            last_year = Some((year, n));
-        }
-    }
-
-    let (last_year_number, last_year) = match last_year {
-        Some(last_year) => last_year,
-        None => return Err(anyhow!("missing last year")),
+    let Some(row) = it.by_ref().skip(12).next() else {
+        return Err(anyhow!("couldn't find header row"));
     };
+
+    let region_index = row
+        .iter()
+        .position(
+            |cell| matches!(cell, Data::String(s) if s == "Region, subregion, country or area *"),
+        )
+        .context("couldn't find region column")?;
+    let year_index = row
+        .iter()
+        .position(|cell| matches!(cell, Data::String(s) if s == "Year"))
+        .context("couldn't find year column")?;
+    let first_index = row
+        .iter()
+        .position(|cell| matches!(cell, Data::Float(v) if *v == 0.0))
+        .context("couldn't find first index")?;
+    let last_index = row
+        .iter()
+        .position(|cell| matches!(cell, Data::String(s) if s == "100+"))
+        .context("couldn't find last index")?;
+
+    ensure!(
+        first_index < last_index,
+        "first index must be less than last index"
+    );
 
     let thousand = Rational::new(1000u32, 1u32);
 
+    let mut last_years = HashMap::<String, (u32, Rational)>::new();
+
     for row in it {
-        let region = if let [Data::Float(..), Data::String(..), Data::String(region), ..] = row {
-            region
-        } else {
+        let region = row
+            .get(region_index)
+            .context("missing_region")?
+            .as_string()
+            .context("region is not a string")?;
+
+        let Some(year) = row
+            .get(year_index)
+            .and_then(|cell| u32::try_from(cell.as_i64()?).ok())
+        else {
             continue;
         };
 
-        let population = match row.get(last_year) {
-            Some(Data::Float(population)) => {
-                Rational::from_f64(*population).unwrap_or_else(|| Rational::new(1, 1)) * &thousand
-            }
-            Some(Data::Int(population)) => Rational::new(*population, 1u32) * &thousand,
-            _ => {
+        let mut population = Rational::new(0u32, 1u32);
+
+        for index in first_index..=last_index {
+            let Some(Data::Float(value)) = row.get(index) else {
                 continue;
-            }
-        };
+            };
+
+            population += Rational::new(*value as u64, 1u32) * &thousand;
+        }
 
         let mut names = Vec::new();
         names.push("population".into());
-        names.extend(analyzer.filter(region));
+        names.extend(analyzer.filter(&region));
+        names.push(year.to_string().into());
 
         db.constants.push(Constant {
             source: Some(SOURCE),
             tokens: names,
-            description: format!("Population of {region} in {last_year_number}").into(),
+            description: format!("Population of {region} in {year}").into(),
+            unit: Default::default(),
+            value: population.clone(),
+        });
+
+        let last_year = last_years
+            .entry(region.to_string())
+            .or_insert_with(|| (0, Rational::new(0u32, 1u32)));
+
+        if year > last_year.0 {
+            *last_year = (year, population);
+        }
+    }
+
+    for (region, (year, population)) in last_years {
+        let mut names = Vec::new();
+
+        names.push("population".into());
+        names.extend(analyzer.filter(&region));
+
+        db.constants.push(Constant {
+            source: Some(SOURCE),
+            tokens: names,
+            description: format!("Population of {region} in {year}").into(),
             unit: Default::default(),
             value: population,
         });
